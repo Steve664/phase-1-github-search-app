0b0240bb0c999a6a764be026a8356825917fdf01
Steve Kariuki <stevekariuki5@gmail.com>
2022-12-07 22:12:26 +0300

    completed assignment

--- js/index.js
+++ js/index.js
@@ -5,16 +5,18 @@ const form = document.getElementById('github-form')
 form.addEventListener('submit', event => {
     event.preventDefault()
     let input = event.target.search.value;
+    //eliminate white space
     let user = input.split(' ').join('')
 
     searchForUser(user)
 })
-
+//fetch user
 function searchForUser(user) {
     fetch(`${link}users/${user}`)
         .then(resp => resp.json())
         .then(data => displayUserInfo(data, user))
 }
+//fetch repos and iterate through
 function fetchRepos(repo) {
     const div2 = document.querySelector("#repos-list")
     repositoryNames = []
@@ -25,17 +27,18 @@ function fetchRepos(repo) {
     }
     div2.style.border = "3px solid #FF0000"
 }
+//display found user on html
 function displayUserInfo(data, user) {
     const div = document.querySelector("#user-list")
 
     const h2 = document.createElement("h2")
     h2.textContent = data.login
-
+    //link on image
     const a = document.createElement("a");
     a.target = "_blank"
     a.href = `https://github.com/${data.login}`
     a.innerHTML = `<img src="${data.avatar_url}"/>`
-
+    //get repos button
     const btn = document.createElement("button")
     btn.setAttribute("id", `${data.login}`)
     btn.textContent = "View Repositories >"
